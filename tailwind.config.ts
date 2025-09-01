a010efc81772bb45ff63752c9416ee434d093a69
naikotain <167589591+naikotain@users.noreply.github.com>
2025-09-01 16:09:17 -0400

    archivo-0109

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ export default {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
+				background: '#ace3fdff',
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
